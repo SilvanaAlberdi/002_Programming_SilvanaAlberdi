e67c056f79d7397f8cf4cdf60c46828ba0ac3f14
SilvanaAlberdi <silvanaalberdi@ni.eus>
2022-10-11 12:50:45 +0200

    Programming 3D

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -12,8 +12,17 @@ public class MyFirstScript : MonoBehaviour
     //This variable saves the player's age
     public int playerAge = 20;
     private float playerSpeed = 3.125f;
+    public int points = 256;
 
     //This variable saves the player's username
     public string playerName = "Silo";
+
+    public string message = "¡Hola mundo!";
     [SerializeField]private bool isGameOver = false;
+
+    private void Start()
+    {
+        Debug.Log(message);
+        Debug.Log(string.Format("Hola {0}, tienes {1} puntos", playerName, points));
+    }
 }
--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
+    // This variab
+    [SerializeField] private int wheels = 4;
+
     // Start is called before the first frame update
     void Start()
     {
