ba45e0046c19f8891aa4092302922776e4bf314b
SilvanaAlberdi <silvanaalberdi@ni.eus>
2022-10-17 10:43:10 +0200

    I hate maths for a reason

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -14,6 +14,9 @@ public class MyFirstScript : MonoBehaviour
     private float playerSpeed = 3.125f;
     public int points = 256;
 
+    public float x = 7;
+    public float y = 4;
+
     //This variable saves the player's username
     public string playerName = "Silo";
 
@@ -24,5 +27,11 @@ public class MyFirstScript : MonoBehaviour
     {
         Debug.Log(message);
         Debug.Log(string.Format("Hola {0}, tienes {1} puntos", playerName, points));
+
+        Debug.Log(x + y);
+        Debug.Log(x - y);
+        Debug.Log(x * y);
+        Debug.Log(x / y);
+        Debug.Log(x % y);
     }
 }
--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -4,12 +4,25 @@ using UnityEngine;
 
 public class Vehicle : MonoBehaviour
 {
-    // This variab
+    /*
+    // This variable, although private, can be edited from the Game editor thanks to the SerializeField command at the very beginning
     [SerializeField] private int wheels = 4;
+    [SerializeField] string name = "Car";
+    */
+
+    
 
     // Start is called before the first frame update
     void Start()
     {
+        /*
+        // Method 1
+        Debug.Log($"{name} tiene {wheels} ruedas");
+
+        // Method 2
+        Debug.Log(string.Format("El vehículo {0} tiene {1} ruedas", name, wheels));
+        */
+
         
     }
 
