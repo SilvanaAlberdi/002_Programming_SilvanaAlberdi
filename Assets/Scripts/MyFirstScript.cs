10585a21cd000c79ea2b296d35da2b7e582612df
SilvanaAlberdi <silvanaalberdi@ni.eus>
2022-10-18 12:51:48 +0200

    Programming

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -10,23 +10,25 @@ public class MyFirstScript : MonoBehaviour
      */
 
     //This variable saves the player's age
-    public int playerAge = 20;
-    private float playerSpeed = 3.125f;
-    public int points = 256;
+    //public int playerAge = 20;
+    //private float playerSpeed = 3.125f;
+    //public int points = 256;
 
     public float x = 7;
     public float y = 4;
 
     //This variable saves the player's username
-    public string playerName = "Silo";
+    //public string playerName = "Silo";
 
-    public string message = "¡Hola mundo!";
-    [SerializeField]private bool isGameOver = false;
+    //public string message = "¡Hola mundo!";
+    //[SerializeField]private bool isGameOver = false;
 
     private void Start()
     {
+        /*
         Debug.Log(message);
         Debug.Log(string.Format("Hola {0}, tienes {1} puntos", playerName, points));
+        */
 
         Debug.Log(x + y);
         Debug.Log(x - y);
